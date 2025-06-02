b9465e18a098c306f1fa8b8fd89b91954274d42d
AndrewBezotchestvo <96548028+AndrewBezotchestvo@users.noreply.github.com>
2025-06-02 16:46:37 +0800

    add level system

--- mathquizz/ConsoleApp1/Program.cs
+++ mathquizz/ConsoleApp1/Program.cs
@@ -17,10 +17,14 @@ namespace ConsoleApp1
 
             Random random = new Random();
 
+            Console.WriteLine("Добро пожаловать в математический квиз");
+            Console.Write("Напишите уровень сложности от 1 до 3: ");
+            int level = int.Parse(Console.ReadLine());
+
             while (true) 
             {
-                a = random.Next(0, 10);
-                b = random.Next(0, 10);
+                a = random.Next(-10 * level, 10 * level);
+                b = random.Next(-10 * level, 10 * level);
 
                 Console.Write($"{a} + {b} = ");
                 string answer = Console.ReadLine();
