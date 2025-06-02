5bb966abe62d82ac8cd1ef331ef8006916e242d3
AndrewBezotchestvo <96548028+AndrewBezotchestvo@users.noreply.github.com>
2025-06-02 16:37:15 +0800

    add project

---
+++ mathquizz/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int record = 0;
+            int a = 0;
+            int b = 0;
+
+            Random random = new Random();
+
+            while (true) 
+            {
+                a = random.Next(0, 10);
+                b = random.Next(0, 10);
+
+                Console.Write($"{a} + {b} = ");
+                string answer = Console.ReadLine();
+
+                if (a + b == int.Parse(answer)) 
+                {
+                    score++;
+                    Console.WriteLine($"Првильно, ваш счет {score}");
+                }
+                else
+                {
+                    Console.WriteLine($"Не правильно, ваш счет {score}");
+                }
+            }
+        }
+    }
+}
